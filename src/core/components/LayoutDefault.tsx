10d7545e1abfdc227ac55d764bc346971c6e5d9f
Weslley <Weslley.Alves.V@outlook.com>
2023-12-06 21:31:50 -0300

    Correção da estrutura inicial

--- src/core/components/LayoutDefault.tsx
+++ src/core/components/LayoutDefault.tsx
@@ -1,21 +1,8 @@
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { ReactNode, useState } from 'react';
-import { AppBar } from '@mui/material';
+import { AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 
 interface IProps {
   children: ReactNode
@@ -33,15 +20,6 @@ export const LayoutDefault = (props : IProps) => {
     setOpen(false);
   }
   
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  }));
-  
   return (
     <Box>
       <CssBaseline />
@@ -55,24 +33,12 @@ export const LayoutDefault = (props : IProps) => {
            </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
+      <Drawer sx={{ width: drawerWidth, '& .MuiDrawer-paper': { width: drawerWidth,}}} variant="persistent" anchor="left" open={open}>
+        <Box display='flex' alignItems='center' justifyContent='flex-end'>
           <IconButton onClick={handleDrawerClose}>
             <ChevronLeftIcon />
           </IconButton>
-        </DrawerHeader>
+        </Box>
         <Divider />
         <List>
           <ListItem disablePadding>
@@ -86,8 +52,8 @@ export const LayoutDefault = (props : IProps) => {
         </List>
         <Divider />
       </Drawer>
-      <Box>
-        {props.children}
+      <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column', marginTop: 4, marginLeft: 2, paddingTop: 5, bgcolor: 'background.paper', borderRadius: 1, }}>
+        {props.children} 
       </Box>
     </Box>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-import { Typography } from "@mui/material"
 import { LayoutDefault } from "../../core/components/LayoutDefault"
 
 export const Home = () => {
     return(
         <LayoutDefault>
-          <Typography>Página inicial</Typography>
+          Página inicial
         </LayoutDefault>
     )
 } 
